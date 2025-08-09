bb9e8196a6aecb3a3375a2ff07e3a547f12bec90
chrisw0987 <wuc0987@gmail.com>
2025-08-09 01:17:35 -0400

    Switch to HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ShopContextProvider from './Context/ShopContext';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ShopContextProvider>
-    <App/>
+  <HashRouter>
+    <ShopContextProvider>
+      <App/>
   </ShopContextProvider>
+  </HashRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
